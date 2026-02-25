4477da1ffba1a9b94433a9658066a9b0c7db4b42
ortymd <dmytro.klobudskyi@gmail.com>
2026-02-25 22:19:41 +0200

    Slot machine update

--- main.py
+++ main.py
@@ -1487,7 +1487,91 @@ print(e) # built-in scope
 
 # 43 (05:38:34) ⭐ slot machine 🎰
 
-print("asdas")
+# import random
+
+# def spin_row():
+#     symbols = ['🍒','🍉','🍋','🔔','⭐']
+
+#     # results = []
+#     # for symbol in range(3):
+#     #     results.append(random.choice(symbols))
+#     # return results
+
+#     return [random.choice(symbols) for _ in range(3) ]
+
+
+# def print_row(row):
+#     print("-------------")
+#     print(" │ ".join(row))
+#     print("-------------")
+
+# def get_payout(row, bet):
+#     if row[0] == row[1] == row[2]:
+#         if row[0] == '🍒':
+#             return bet * 2
+#         elif row[0] == '🔔':
+#             return bet * 3
+#         elif row[0] == '🍉':
+#             return bet * 4
+#         elif row[0] == '⭐':
+#             return bet * 5
+#         elif row[0] == '🍋':
+#             return bet * 6
+#     return 0
+
+# def main():
+#     balance = 100
+
+#     print("-----------------------")
+#     print("Welcome to Python Slots")
+#     print("-----------------------")
+#     print("Symbols:🍒🍉🍋🔔⭐")
+
+#     while balance > 0:
+#         print("-----------------------")
+#         print(f"Current balance: ${balance}")
+
+#         bet = input("Place your bet amount: ")
+
+#         if not bet.isdigit():
+#             print("Please enter a valid number")
+#             continue
+
+#         bet = int(bet)
+
+#         if bet > balance:
+#             print("Insufficient funds")
+#             continue
+
+#         if bet <= 0:
+#             print("Bet must be greater than 0")
+#             continue
+
+#         balance -= bet
+
+#         row = spin_row()
+#         print("Spinning ...")
+#         print_row(row)
+
+#         payout = get_payout(row, bet)
+
+#         if payout > 0:
+#             print(f"You won ${payout}")
+#         else:
+#             print("Sorry, you lost this round")
+
+#         balance += payout
+
+#         play_again = input("Do you want to spin again? (Y/N):").upper()
+
+#         if play_again != 'Y':
+#             break
+#     print("--------------------------------------------")
+#     print(f"Game over! Your final balance is ${balance}")
+#     print("--------------------------------------------")
+
+# if __name__ == '__main__':
+#     main()
 
 # 44 (05:58:45) ⭐ encryption program 🔐
 # 45 (06:07:26) ⭐ hangman game 🕺
