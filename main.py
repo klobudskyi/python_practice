8ea4cc0875a45a6524784ded521e9169c3ad58f4
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-04 19:26:10 +0200

    Encryption program update

--- main.py
+++ main.py
@@ -1574,6 +1574,43 @@ print(e) # built-in scope
 #     main()
 
 # 44 (05:58:45) ⭐ encryption program 🔐
+
+# import random
+# import string
+
+# chars = " " + string.punctuation + string.digits + string.ascii_letters
+# chars = list(chars)
+# key = chars.copy()
+
+# random.shuffle(key)
+
+# # print(f"chars: {chars}")
+# # print(f"key: {key}")
+
+# # Encryption
+
+# plain_text = input("Enter a message to encrypt: ")
+# cipher_text = ""
+
+# for letter in plain_text:
+#     index = chars.index(letter)
+#     cipher_text += key[index]
+
+# print(f"original message: {plain_text}")
+# print(f"encrypted message: {cipher_text}")
+
+# # Decryption
+
+# cipher_text = input("Enter a message to decrypt: ")
+# plain_text = ""
+
+# for letter in cipher_text:
+#     index = key.index(letter)
+#     plain_text += chars[index]
+
+# print(f"encrypted message: {cipher_text}")
+# print(f"original message: {plain_text}")
+
 # 45 (06:07:26) ⭐ hangman game 🕺
 # 46 (06:32:32) python object oriented programming 🚗
 # 47 (06:44:50) class variables 🎓
