5a90d480c4602c73b315979e874827ba3a34e3f8
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-10 18:17:22 +0200

    Multiple inheritance update

--- main.py
+++ main.py
@@ -1828,6 +1828,58 @@ print(e) # built-in scope
 # mouse.speak()
 
 # 49 (07:00:02) multiple inheritance 🐟
+
+# multiple inheritance = inherit from more than one parent class
+#                        C(A, B)
+
+# multilevel inheritance = inherit from a parent which inherits from another parent
+#                          C(B) <- B(A) <- A
+
+# class Animal:
+
+#     def __init__(self, name):
+#         self.name = name
+
+#     def eat(self):
+#         print(f"{self.name} is eating")
+
+#     def sleep(self):
+#         print(f"{self.name} is sleeping")
+
+# class Prey(Animal):
+#     def flee(self):
+#         print(f"{self.name} is fleeing")
+
+# class Predator(Animal):
+#     def hunt(self):
+#         print(f"{self.name} is hunting")
+
+# class Rabbit(Prey):
+#     pass
+
+# class Hawk(Predator):
+#     pass
+
+# class Fish(Prey, Predator):
+#     pass
+
+# rabbit = Rabbit("Bugs")
+# hawk = Hawk("Tony")
+# fish = Fish("Nemo")
+
+# rabbit.flee()
+# rabbit.eat()
+# rabbit.sleep()
+
+# hawk.hunt()
+# hawk.eat()
+# hawk.sleep()
+
+# fish.flee()
+# fish.hunt()
+# fish.eat()
+# fish.sleep()
+
 # 50 (07:08:04) super() 🔴
 # 51 (07:21:10) polymorphism 🎭
 # 52 (07:29:15) duck typing 🦆
