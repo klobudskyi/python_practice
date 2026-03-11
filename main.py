8e34d6583c0bc46ddc2315e1e975ad7b7864109d
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-11 06:43:27 +0200

    super() function update

--- main.py
+++ main.py
@@ -1881,6 +1881,70 @@ print(e) # built-in scope
 # fish.sleep()
 
 # 50 (07:08:04) super() 🔴
+
+# super() = Function used in a child class to call methods from a parent class (superclass)
+#           Allows you to extend the functionality of the inherited methods
+
+# import math
+
+# class Shape:
+#     def __init__(self, color, is_filled):
+#         self.color = color
+#         self.is_filled = is_filled
+
+#     def describe(self):
+#         print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+# class Circle(Shape):
+#     def __init__(self, color, is_filled, radius):
+#         super().__init__(color, is_filled)
+#         self.radius = radius
+
+#     def describe(self): # method overriding
+#         super().describe() # extending the functionality of the describe method
+#         print(f"It is a circle with an area of {math.pi * self.radius ** 2:.2f} cm^2")
+        
+
+# class Square(Shape):
+#     def __init__(self, color, is_filled, width):
+#         super().__init__(color, is_filled)
+#         self.width = width
+
+#     def describe(self): # method overriding
+#         super().describe() # extending the functionality of the describe method
+#         print(f"It is a square with an area of {self.width ** 2:.2f} cm^2")
+
+# class Triangle(Shape):
+#     def __init__(self, color, is_filled, width, height):
+#         super().__init__(color, is_filled)
+#         self.width = width
+#         self.height = height
+
+#     def describe(self): # method overriding
+#         super().describe() # extending the functionality of the describe method
+#         print(f"It is a triangle with an area of {self.width * self.height / 2:.2f} cm^2")
+
+# circle = Circle(color="red", is_filled=True, radius=5)
+# square = Square(color="blue", is_filled=False, width=6)
+# triangle = Triangle(color="yellow", is_filled=True, width=7, height=8)
+
+# print(circle.color)
+# print(circle.is_filled)
+# print(f"{circle.radius} cm")
+
+# print(square.color)
+# print(square.is_filled)
+# print(f"{square.width} cm")
+
+# print(triangle.color)
+# print(triangle.is_filled)
+# print(f"{triangle.width} cm")
+# print(f"{triangle.height} cm")
+
+# circle.describe()
+# square.describe()
+# triangle.describe()
+
 # 51 (07:21:10) polymorphism 🎭
 # 52 (07:29:15) duck typing 🦆
 # 53 (07:33:34) static methods ⚡
