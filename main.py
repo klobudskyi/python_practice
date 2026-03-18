00621f7f3d640e95dcc3805f36947afa9ce87e48
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-18 10:14:05 +0200

    Alarm clock update

--- main.py
+++ main.py
@@ -2378,6 +2378,38 @@ print(e) # built-in scope
 #     print("Target date has NOT PASSED")
 
 # 63 (08:54:46) ⭐ alarm clock ⏰
+
+# import time
+# import datetime
+# import pygame
+
+# def set_alarm(alarm_time):
+#     print(f"Alarm set for {alarm_time}")
+#     sound_file = "python_practice\\Nebula - The Grey Room _ Density & Time.mp3"
+#     is_running = True
+
+#     while is_running:
+#         current_time = datetime.datetime.now().strftime("%H:%M:%S")
+#         print(current_time)
+
+#         if current_time == alarm_time:
+#             print("Wake up!😴")
+
+#             pygame.mixer.init()
+#             pygame.mixer.music.load(sound_file)
+#             pygame.mixer.music.play()
+
+#             while pygame.mixer.music.get_busy():
+#                 time.sleep(1)
+            
+#             is_running = False
+
+#         time.sleep(1)
+
+# if __name__ == "__main__":
+#     alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+#     set_alarm(alarm_time)
+
 # 64 (09:05:03) multithreading 🧵
 # 65 (09:13:45) request API data ↩️
 # 66 (09:22:19) PyQt5 GUI intro 🖥️
