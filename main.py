0bd1f199f20502e7f1e3426c8ea3c42580b86267
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-19 16:41:29 +0200

    PyQt5 labels update

--- main.py
+++ main.py
@@ -2495,23 +2495,46 @@ print(e) # built-in scope
 
 # 67 (09:31:27) PyQt5 labels 🏷️
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+# from PyQt5.QtGui import QFont
+# from PyQt5.QtCore import Qt
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(700, 300, 500, 500) # x, y, width, height
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setGeometry(700, 300, 500, 500) # x, y, width, height
 
+#         label = QLabel("Hello", self)
+#         label.setFont(QFont("Arial", 30))
+#         label.setGeometry(0, 0, 500, 100)
+#         label.setStyleSheet("color: #f61052;"
+#                             "background-color: #292929;"
+#                             "font-weight: bold;"
+#                             "font-style: italic;"
+#                             "text-decoration: underline;")
+        
+        # label.setAlignment(Qt.AlignTop) # vertically top
+        # label.setAlignment(Qt.AlignBottom) # vertically bottom
+        # label.setAlignment(Qt.AlignVCenter) # vertically center
 
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+        # label.setAlignment(Qt.AlignRight) # horizontally right
+        # label.setAlignment(Qt.AlignHCenter) # horizontally center
+        # label.setAlignment(Qt.AlignLeft) # horizontally left
 
-if __name__ == "__main__":
-    main()
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignTop) # center & top
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom) # center & bottom
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # center & center
+        # label.setAlignment(Qt.AlignCenter) # center & center
+
+# def main():
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
+
+# if __name__ == "__main__":
+#     main()
 
 # 68 (09:40:23) PyQt5 images 📷
 # 69 (09:46:28) PyQt5 layout managers 🧲
