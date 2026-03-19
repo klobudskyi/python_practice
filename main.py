bbcc5c1aaf3399ef85f57bf06d2df62c62021720
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-19 16:41:17 +0200

    PyQt5 labels update

--- main.py
+++ main.py
@@ -2496,15 +2496,13 @@ print(e) # built-in scope
 # 67 (09:31:27) PyQt5 labels 🏷️
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My first GUI")
         self.setGeometry(700, 300, 500, 500) # x, y, width, height
-        self.setWindowIcon(QIcon("python_practice\\prison_mike.jpg"))
+
 
 def main():
     app = QApplication(sys.argv)
