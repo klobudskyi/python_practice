f69d091c37ab546d00122ca9f4c448a631ac64eb
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-21 11:42:04 +0200

    PyQt5 layout managers update

--- main.py
+++ main.py
@@ -2571,23 +2571,70 @@ print(e) # built-in scope
 
 # 69 (09:46:28) PyQt5 layout managers 🧲
 
-import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
-                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+# import sys
+# from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+#                              QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
 
-class MainWidnow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(700, 300, 500, 500)
+# class MainWidnow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setGeometry(700, 300, 500, 500)
+#         self.initUI()
 
-def main():
-    app = QApplication(sys.argv)
-    window = MainWidnow()
-    window.show()
-    sys.exit(app.exec_())
+#     def initUI(self):
+#         central_widget = QWidget()
+#         self.setCentralWidget(central_widget)
 
-if __name__ == "__main__":
-    main()
+#         label1 = QLabel("#1", self)
+#         label2 = QLabel("#2", self)
+#         label3 = QLabel("#3", self)
+#         label4 = QLabel("#4", self)
+#         label5 = QLabel("#5", self)
+
+#         label1.setStyleSheet("background-color: red;")
+#         label2.setStyleSheet("background-color: yellow;")
+#         label3.setStyleSheet("background-color: green;")
+#         label4.setStyleSheet("background-color: blue;")
+#         label5.setStyleSheet("background-color: purple;")
+
+        # vbox = QVBoxLayout()
+
+        # vbox.addWidget(label1)
+        # vbox.addWidget(label2)
+        # vbox.addWidget(label3)
+        # vbox.addWidget(label4)
+        # vbox.addWidget(label5)
+
+        # central_widget.setLayout(vbox)
+
+        # hbox = QHBoxLayout()
+
+        # hbox.addWidget(label1)
+        # hbox.addWidget(label2)
+        # hbox.addWidget(label3)
+        # hbox.addWidget(label4)
+        # hbox.addWidget(label5)
+
+        # central_widget.setLayout(hbox)
+
+        # grid = QGridLayout()
+
+        # grid.addWidget(label1, 0, 0)
+        # grid.addWidget(label2, 0, 1)
+        # grid.addWidget(label3, 1, 0)
+        # grid.addWidget(label4, 1, 1)
+        # grid.addWidget(label5, 2, 0)
+
+        # central_widget.setLayout(grid)
+
+# def main():
+#     app = QApplication(sys.argv)
+#     window = MainWidnow()
+#     window.show()
+#     sys.exit(app.exec_())
+
+# if __name__ == "__main__":
+#     main()
 
 # 70 (09:53:07) PyQt5 buttons 🛎️
 # 71 (10:00:12) PyQt5 checkboxes ✅
