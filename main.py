883e3165118a061ee95caebc0d66d7e588bc09fb
ortymd <dmytro.klobudskyi@gmail.com>
2026-03-24 07:45:57 +0200

    PyQt5 checkboxes update

--- main.py
+++ main.py
@@ -2666,6 +2666,38 @@ print(e) # built-in scope
 #     sys.exit(app.exec_())
 
 # 71 (10:00:12) PyQt5 checkboxes ✅
+
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+# from PyQt5.QtCore import Qt
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setGeometry(700, 300, 500, 500)
+#         self.checkbox = QCheckBox("Do you like food?", self)
+#         self.initUI()
+
+#     def initUI(self):
+#         self.checkbox.setGeometry(10, 0, 500, 100)
+#         self.checkbox.setStyleSheet("font-size: 30px;"
+#                                     "font-family: Arial;")
+#         self.checkbox.setChecked(False)
+#         self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+#     def checkbox_changed(self, state):
+#         if state == Qt.Checked:
+#             print("You like food")
+#         else:
+#             print("You do not like food")
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
+
 # 72 (10:06:42) PyQt5 radio buttons 🔘
 # 73 (10:15:55) PyQt5 line edits 💬
 # 74 (10:21:55) PyQt5 CSS styles 🎨
