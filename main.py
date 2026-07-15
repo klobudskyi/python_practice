bf8ee67825e4913a662450f8755bd8277b814258
ortymd <dmytro.klobudskyi@gmail.com>
2026-07-15 11:18:37 +0300

    PyQt5 line edits update

--- main.py
+++ main.py
@@ -2747,17 +2747,42 @@ print(e) # built-in scope
 #         if radio_button.isChecked():
 #             print(f"{radio_button.text()} is selected")
 
+# 73 (10:15:55) PyQt5 line edits 💬
+
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setGeometry(700, 300, 500, 500)
+#         self.line_edit = QLineEdit(self)
+#         self.button = QPushButton("Submit", self)
+#         self.initUI()
+
+#     def initUI(self):
+#        self.line_edit.setGeometry(10, 10, 200, 40)
+#        self.button.setGeometry(210, 10, 100, 40)
+#        self.line_edit.setStyleSheet("font-size: 25px; font-family: Arial")
+#        self.button.setStyleSheet("font-size: 25px; font-family: Arial")
+#        self.line_edit.setPlaceholderText("Enter your name")
+       
+#        self.button.clicked.connect(self.submit)
+
+#     def submit(self):
+#         text = self.line_edit.text()
+#         print(f"Hello {text}!")
+
 # if __name__ == "__main__":
 #     app = QApplication(sys.argv)
 #     window = MainWindow()
 #     window.show()
 #     sys.exit(app.exec_())
 
-# 73 (10:15:55) PyQt5 line edits 💬
-
-
-
 # 74 (10:21:55) PyQt5 CSS styles 🎨
+
+
+
 # 75 (10:32:48) ⭐ digital clock program 🕒
 # 76 (10:48:38) ⭐ stopwatch program ⏱
 # 77 (11:06:05) ⭐ weather API app ☀️
