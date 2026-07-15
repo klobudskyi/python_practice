e0b978d757a00915929c0b5bcdec5c59855a376a
ortymd <dmytro.klobudskyi@gmail.com>
2026-07-15 13:11:11 +0300

    Digital clock program update

--- main.py
+++ main.py
@@ -2850,5 +2850,47 @@ print(e) # built-in scope
 #     sys.exit(app.exec_())
 
 # 75 (10:32:48) ⭐ digital clock program 🕒
+
+# import sys
+# from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+# from PyQt5.QtCore import QTimer, QTime, Qt
+
+# class DigitalClock(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.time_label = QLabel(self)
+#         self.timer = QTimer(self)
+#         self.initUI()
+
+#     def initUI(self):
+#         self.setWindowTitle("Digital Clock")
+#         self.setGeometry(600, 400, 300, 100)
+
+#         vbox = QVBoxLayout()
+#         vbox.addWidget(self.time_label)
+#         self.setLayout(vbox)
+
+#         self.time_label.setAlignment(Qt.AlignCenter)
+#         self.time_label.setStyleSheet("font-size: 150px; font-family: Arial; color: hsl(111, 100%, 50%);")
+#         self.setStyleSheet("background-color: black;")
+
+#         self.timer.timeout.connect(self.update_time)
+#         self.timer.start(1000)
+
+#         self.update_time()
+
+#     def update_time(self):
+#         current_time = QTime.currentTime().toString("hh:mm:ss")
+#         self.time_label.setText(current_time)
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     clock = DigitalClock()
+#     clock.show()
+#     sys.exit(app.exec_())
+
 # 76 (10:48:38) ⭐ stopwatch program ⏱
+
+
+
 # 77 (11:06:05) ⭐ weather API app ☀️
